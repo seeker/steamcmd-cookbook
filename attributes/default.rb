da6470f54905031f23feb2f74e95232297d9b199
Nicholas Wright <dozedoffagain@gmail.com>
2017-07-15 13:31:24 +0200

    Added default attributes

---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+# Username and password for the steam account. NOTE: some actions cannot be performed as anonymous 
+default['steamcmd']['steam']['username'] = 'anonymous'
+default['steamcmd']['steam']['password'] = nil
